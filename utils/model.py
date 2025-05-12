8d88575265f6b035c6ea0e4624fb8a73a30ad1d7
jk2kk <pmucha661@gmail.com>
2025-05-12 09:30:43 +0200

    wip: GUI

--- utils/model.py
+++
@@ -1,7 +0,0 @@
-users = [
-    {"name": "Daniel", "location": "Łódź", "posts": 500},
-    {"name": "Michał", "location": "Krasnystaw", "posts": 400},
-    {"name": "Ksavier", "location": "Grudziądz", "posts": 300},
-    {"name": "Damian", "location": "Kraków", "posts": 200},
-
-]
